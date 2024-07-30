6125bcee4a0efef07ca3005575689a271342452b
KIMURA MASAYUKI <>
2024-07-30 16:35:12 +0900

    ♻️ ナビゲーション修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,17 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{asset('img/dog.png')}}" style="max-height:80px;">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('question.index')" :active="request()->routeIs('question.index')">
+                        HOME
+                    </x-nav-link>
+                    <x-nav-link :href="route('question.create')" :active="request()->routeIs('question.create')">
+                        新規作成
                     </x-nav-link>
                 </div>
             </div>
@@ -68,8 +71,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('question.index')" :active="request()->routeIs('question.index')">
+                HOME
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('question.create')" :active="request()->routeIs('question.create')">
+                新規作成
             </x-responsive-nav-link>
         </div>
 
